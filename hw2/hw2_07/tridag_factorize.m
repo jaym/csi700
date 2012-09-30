5e7a76d69994206648ad6174e81d9dc42b537e0f
Jay <jdmundrawala@gmail.com>
2012-09-30 14:08:25 -0400

    tridag_solve can solve matrix z. HW 7 finished

--- hw2/hw2_07/tridag_factorize.m
+++ hw2/hw2_07/tridag_factorize.m
@@ -1,7 +1,7 @@
 function [l, u] = tridiag_factorize(a, b, c)
     u(1) = b(1);
     for i = 2:size(b,1)
-        l(i-1) = a(i-1)/u(i-1);
-        u(i) = b(i) - l(i-1)*c(i-1)
+        l(i-1) = a(i-1)/u(i-1);;
+        u(i) = b(i) - l(i-1)*c(i-1);
     end
 end
--- hw2/hw2_07/tridag_solve.m
+++ hw2/hw2_07/tridag_solve.m
@@ -1,14 +1,15 @@
 %%Solve Ax = z
 function x = tridag_solve(l, u, c, z)
+    for col = 1:size(z,2)
 %% Solve Ly=z
-    y = zeros(size(z,1),1);
-    y(1) = z(1);
-    for i = 2:size(z,1)
-        y(i) = z(i) - l(i-1)*y(i-1);
-    end
+        y(1) = z(1,col);
+        for i = 2:size(z,1)
+            y(i) = z(i,col) - l(i-1)*y(i-1);
+        end
 %%Solve Ux = y
-    x(size(z,1)) = y(size(z,1))/u(size(z,1));
-    for i = size(z,1)-1:-1:1
-        x(i) = (y(i) - c(i)*x(i+1))/u(i);
+        x(size(z,1),col) = y(size(z,1))/u(size(z,1));
+        for i = size(z,1)-1:-1:1
+            x(i,col) = (y(i) - c(i)*x(i+1,col))/u(i);
+        end
     end
 end
